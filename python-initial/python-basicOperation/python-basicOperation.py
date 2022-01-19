5991502509a7a9ac845b82c632fc66e14c83dcfc
Filizzola <juliofilizzola@hotmail.com>
2022-01-19 08:25:10 -0300

    🎉: adicionando mais exemplos

--- python-initial/python-basicOperation/python-basicOperation.py
+++ python-initial/python-basicOperation/python-basicOperation.py
@@ -1,3 +1,8 @@
 print(2 * 3)
 print(2 + 3)
 print(3 / 2)
+print(3 // 2)
+
+square_root = 25 ** (1/2)  # raiz quadrada de 25. O operador `**` significa "elevado a"
+
+print(square_root + 1)  # saída: 6.0
